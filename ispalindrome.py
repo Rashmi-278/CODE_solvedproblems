baf91a4e5251c61e99a07f42fb397d39aca26632
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2019-08-02 00:25:38 +0530

    Create ispalindrome.py

---
+++ ispalindrome.py
@@ -0,0 +1,20 @@
+s="was it a cat i saw?"
+s=''.join([i for i in s if i.isalpha()]).replace(" ","").lower()
+print(s==s[::-1])
+print(s )
+
+def ispalindrome(s):
+    i=0
+    j=len(s)-1
+
+    while i < j:
+        while not s[i].isalnum() and i < j:
+            i+=1
+        while not s[j].isalnum() and i < j:
+            j-=1
+        if s[i].lower()!=s[j].lower():
+            return False
+        i+=1
+        j-=1
+    return True
+print(ispalindrome(s))
