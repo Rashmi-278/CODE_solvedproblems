46c7f55a11c6f11d3613eac7f079295ccae3544c
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2019-08-13 23:02:58 +0530

    Create splithouses.py

---
+++ splithouses.py
@@ -0,0 +1,9 @@
+#https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/split-house-547be0e9/
+T = int(input())
+s =input()
+if "HH" in s:
+    
+        print("NO")
+else:
+        print("YES")
+        print(s.replace(".","B"))
